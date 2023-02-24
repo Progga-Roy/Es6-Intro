ef7927169c7076d8430b9c7c9f55ff932701d238
Progga <proggaroy2000@gmail.com>
2023-02-24 15:57:02 +0600

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,26 @@
+const fish = {
+name : 'Hilsha Fish',
+address : 'Chadpur',
+color: 'silver',
+phone : '090944123',
+price : 4000
+
+
+}
+
+
+
+const {phone} = fish;
+console.log(phone);
+// Array destructuring
+const nayoks = ['Sha Rukh Khan','Alomgir','Jeet','Hrittik'] ;
+ const  [a,b,c,d]= nayoks;
+ console.log(d);
+
+//  function destructuring
+ function getNames(){
+    return [] = ['Uttam','Gautam'];
+
+ }
+ const [baba,kaku] = getNames();
+ console.log(kaku,baba);
