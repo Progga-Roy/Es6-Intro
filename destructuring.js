a14583d33489e556a492f56b92e8d7dc26e6a586
Progga <proggaroy2000@gmail.com>
2023-02-24 17:39:12 +0600

    spread operator

--- destructuring.js
+++ destructuring.js
@@ -11,11 +11,11 @@ price : 4000
 
 
 const {phone} = fish;
-console.log(phone);
+// console.log(phone);
 // Array destructuring
 const nayoks = ['Sha Rukh Khan','Alomgir','Jeet','Hrittik'] ;
  const  [a,b,c,d]= nayoks;
- console.log(d);
+//  console.log(d);
 
 //  function destructuring
  function getNames(){
@@ -23,4 +23,9 @@ const nayoks = ['Sha Rukh Khan','Alomgir','Jeet','Hrittik'] ;
 
  }
  const [baba,kaku] = getNames();
- console.log(kaku,baba);
+//  console.log(kaku,baba);
+
+//  spread operator
+const ages = [22,34,56,12,7,24,90]
+const newAges = [...ages,101,105];
+console.log(newAges);
